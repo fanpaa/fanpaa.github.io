94cf461bea7120e132f1418eaebf20bd9b13c5f9
yaohua.fan <yaohua.fan@dcfservice.net>
2015-01-23 11:51:47 +0800

    commit

---
+++ j.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?373c6d0a13efbe4a5ff1e6cc4fbf9f50";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
